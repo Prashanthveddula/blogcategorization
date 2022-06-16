707bede030345b35fb393aef83eeac9eb901aee9
Prashanth <prashanthveddula777@gmail.com>
2022-06-16 13:07:35 +0530

    Added a new method to main.py to extract nationalities

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import nltk
 import os
 from nltk import tokenize
 
+
 #open files
 f1 = open("docs/article1.txt")
 f2 = open("docs/countries.txt")
@@ -20,5 +21,19 @@ def extract_countries(text, CountriesList):
     return res
 
 
+#return a list of all country names and nationalities('Ameriacan', 'Indian')
+def extract_countries2(text): 
+    sent = nltk.tokenize.wordpunct_tokenize(text)
+    pos_tag = nltk.pos_tag(sent)
+    nes = nltk.ne_chunk(pos_tag)
+    places = []
+    for ne in nes:
+        if type(ne) is nltk.tree.Tree:
+            if (ne.label() == 'GPE'):
+                places.append(u' '.join([i[0] for i in ne.leaves()]))
+        
+    return places
+
+
 if __name__ == '__main__':
-    print(extract_countries(article, countries))
+    print(extract_countries2(article))
