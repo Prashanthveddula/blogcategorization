c86fb3157850c673452a53ae4b23c5b25cb7ded1
Parzival17 <52356527+Prashanthveddula@users.noreply.github.com>
2022-06-20 14:14:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,20 +14,8 @@ Chinese = open("docs/China.txt", 'r').read()
 Russian = open("docs/Russia.txt", 'r').read()
 
 
-#returns a list of all country names from input text.
-def extract_countries(text, CountriesList):
-    res = []
-    sent = nltk.word_tokenize(text)
-    pos_tag = nltk.pos_tag(sent)
-    
-    for (word, pos) in pos_tag:
-        if (pos[:2] == 'NN') and (word in CountriesList):
-            res.append(word) 
-    return res
-
-
 #return a list of all country names and nationalities('Ameriacan', 'Indian')
-def extract_countries2(text): 
+def extract_countries(text): 
     sent = nltk.tokenize.word_tokenize(text)
     pos_tag = nltk.pos_tag(sent)
     nes = nltk.ne_chunk(pos_tag)
@@ -38,6 +26,7 @@ def extract_countries2(text):
                 places.append(u' '.join([i[0] for i in ne.leaves()]))
         
     return places
+
 
 #returns a list of all humans names in text
 def extract_names(text):
@@ -54,8 +43,7 @@ def extract_names(text):
 
 
 if __name__ == '__main__':
-    print(extract_countries(article, countries))
-    print(extract_countries2(article))
+    print(extract_countries(article))
     print(extract_names(Indian))
     print(extract_names(American))
     print(extract_names(Chinese))
