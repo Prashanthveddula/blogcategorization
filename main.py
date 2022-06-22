7e8312bfaa0e351352a0137e03896401eb4348c6
Prashanth <prashanthveddula777@gmail.com>
2022-06-23 00:15:38 +0530

    Added method to find coutry from human names

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import nltk
-from nltk.tree import Tree
 
 
 nltk.download("punkt")
@@ -15,10 +14,18 @@ Indian = open("docs/India.txt", "r").read()
 American = open("docs/USA.txt", 'r').read()
 Japanese = open("docs/Japan.txt", 'r').read()
 Russian = open("docs/Russia.txt", 'r').read()
+article2 = open("docs/test.txt", 'r').read()
+dict = {}
+with open("docs/name2lang.txt") as f:
+    for line in f:
+        temp = line.split(",")
+        x = temp[0].strip()
+        y = temp[1].strip()
+        dict[x] = y
 
 
 
-# return a list of all country names and nationalities('Ameriacan', 'Indian')
+# Return a list of all country names and nationalities('Ameriacan', 'Indian')
 def extract_countries(text):
     sent = nltk.tokenize.word_tokenize(text)
     pos_tag = nltk.pos_tag(sent)
@@ -26,25 +33,49 @@ def extract_countries(text):
     places = []
 
     for ne in nes:
-        if type(ne) is nltk.tree.Tree:
+        if type(ne) == nltk.tree.Tree:
             if ne.label() == "GPE":
                 places.append(u" ".join([i[0] for i in ne.leaves()]))
 
     return places
 
 
-# returns a list of all humans names in text
+# Returns a list of all humans names in text
 def extract_names(text):
     sent = nltk.word_tokenize(text)
     pos_tag = nltk.pos_tag(sent)
     nes = nltk.ne_chunk(pos_tag)
     res = []
     for ne in nes:
-        if type(ne) == Tree:
+        if type(ne) == nltk.tree.Tree:
             if ne.label() == "PERSON":
                 res.append(u" ".join([i[0] for i in ne.leaves()]))
 
     return res
+
+
+#Extracts humans names and returns their respective country
+def name_to_country(text):
+    names = []
+    nametoc = []
+    nes = nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(text)))
+    for n in nes:
+        if type(n) == nltk.tree.Tree:
+            if n.label() == "PERSON":
+                name = f" ".join(i[0] for i in n.leaves())
+                names.append(name)
+    for j in names:
+        temp = j.split(" ")
+        found = False
+        for k in temp:
+            if k in dict:
+                nametoc.append((j, dict[k]))
+                found = True
+                break
+        if found == False:
+            nametoc.append((j, 'Not identified'))
+    return nametoc
+
 
 #pytest for extracting names
 def test_answer1():
@@ -56,8 +87,11 @@ def test_answer3():
 def test_answer4():
     assert extract_names(Russian) == ['Garry', 'Kimovich Kasparov']
 
+
+#driver code
 if __name__ == "__main__":
     print(extract_names(Indian))
     print(extract_names(American))
     print(extract_names(Japanese))
     print(extract_names(Russian))
+    print(name_to_country(article2))
